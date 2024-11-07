d6ba5d561250a4d7e1fa1e74e5fc9f4eb8a78d68
ellapiriyeva <ella.piriyeva@gmail.com>
2024-11-07 23:52:59 +0400

    Find Max Number

--- src/main/java/Array/MaxNumber.java
+++ src/main/java/Array/MaxNumber.java
@@ -1,11 +1,16 @@
 package Array;
 
+import java.util.Scanner;
+
 public class MaxNumber {
     public static void main(String[] args){
-        int[] num = {5, 7, 3, 14, 65, 0};
+        int[] num = new int[6];
+        Scanner scan = new Scanner(System.in);
 
+        for(int i = 0; i < num.length; i++){
+            num[i] = scan.nextInt();
+        }
         int max = num[0];
-
         for(int i = 0; i < num.length; i++){
             if(max < num[i]){
                 max = num[i];
